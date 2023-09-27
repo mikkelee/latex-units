4a688f8cb3084b5229e8a674ffeec74c2efd0834
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2023-09-27 17:23:02 +0200

    package.sh: include docs/.sty

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 cd ..
 zip -r non-decimal-units/non-decimal-units-$(date '+%Y%m%d').zip\
 	non-decimal-units/latex/*.{sty,csv,tex}\
-	non-decimal-units/docs/*.{tex,pdf,bib,txt}\
+	non-decimal-units/docs/*.{tex,pdf,bib,txt,sty}\
 	non-decimal-units/README.md
 cd -
